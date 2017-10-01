ec671bb00f66029b7707fdea52f4963a74183ccf
Timothy Nott <timothy@ffrf.org>
2017-10-01 11:20:37 -0500

    for got to add to last commit

--- routes/fetch.js
+++ routes/fetch.js
@@ -2,30 +2,44 @@ const express = require('express')
 const router = express.Router()
 
 const Queue = require('../models/queue')
+const Event = require('../models/event')
 
 const helpers = require('../helpers')
 
+process
 router.get('/', (req, res) => {
   // todo: get active events, for each get reactions, drop into queue
-  let itemID = helpers.getItemIdFromURL('https://www.facebook.com/4ffrf/videos/10155842254339728/')
-  new Queue({'event_id': 1}).save()
-    .then(model => {
-      helpers.getReactions(itemID)
-      .then(reactions => {
-        let aggregate = helpers.aggregateReactions(reactions)
-        model.save({data: JSON.stringify(aggregate)}, {patch: true})
-        res.send({
-          'result': 'SUCCESS',
-          'data': aggregate
-        })
-      })
-      .catch(error => {
-        console.log('wtf, mate?')
-        res.send({
-          'result': 'FAILURE',
-          'error': error
-        })
-      })
+  helpers.getLiveEvents()
+    .then(events => {
+      let currentEvent = {}
+      // console.log(events)
+      for (let i = 0; i < events.length; i++) {
+        currentEvent = events.models[i]
+        console.log(currentEvent.get('id'))
+        let itemID = helpers.getItemIdFromURL(currentEvent.get('url'))
+        new Queue({'event_id': currentEvent.get('id')}).save()
+          .then(model => {
+            helpers.getReactions(itemID)
+              .then(reactions => {
+                let aggregate = helpers.aggregateReactions(reactions)
+                model.save({data: JSON.stringify(aggregate)}, {patch: true})
+                // res.send({
+                //   'result': 'SUCCESS',
+                //   'data': aggregate
+                // })
+              })
+              .catch(error => {
+                console.log('wtf, mate?')
+                // res.send({
+                //   'result': 'FAILURE',
+                //   'error': error
+                // })
+              })
+          })
+      }
+
     })
+
+
 })
 module.exports = router
