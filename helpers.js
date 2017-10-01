b2a9a18e86f9480436adcc18a1119110c7c0cac5
Timothy Nott <timothy@ffrf.org>
2017-10-01 10:21:59 -0500

    writing data to queue table

--- helpers.js
+++ helpers.js
@@ -1,13 +1,31 @@
 const axios = require('axios')
 const accessToken = '350787432040190|2tcZ-CcIcjvAjRn34FrmCgE1Yuw'
+const reactionTypes = ['NONE', 'LIKE', 'LOVE', 'WOW', 'HAHA', 'SAD', 'ANGRY', 'THANKFUL', 'PRIDE']
 helpers = {
   test: function() {
     return 'go!'
   },
-  fixAccessTokenInURL:function(url) {
+  getItemIdFromURL: function(url) {
+    // example https://www.facebook.com/4ffrf/videos/10155842254339728/
+    let test = url.split('/')
+    // test for URLs without trailing slash and return ID portion of path
+    return (test[test.length -1] !== '') ? test[test.length - 1] : test[test.length - 2]
+  },
+  aggregateReactions: function(reactions) {
+    let aggregate = {}
+    // iniitalize data
+    for (let i = 0; i < reactionTypes.length; i++) {
+      aggregate[reactionTypes[i]] = 0
+    }
+    for(let i = 0; i < reactions.length; i++) {
+      aggregate[reactions[i].type]++
+    }
+    return aggregate
+  },
+  fixAccessTokenInURL: function(url) {
     return url.replace(/\u00257C/,"|")
   },
-  getReactions:function(itemID) {
+  getReactions: function(itemID) {
     console.log('starting getReactions')
     let reactions = []
     let starterUrl = 'https://graph.facebook.com/v2.10/' + itemID + '/reactions?access_token=' + accessToken + '&limit=10'
--- routes/fetch.js
+++ routes/fetch.js
@@ -1,20 +1,30 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router()
+
+const Queue = require('../models/queue')
+
 const helpers = require('../helpers')
-// https://www.facebook.com/4ffrf/videos/10155842254339728/
+
 router.get('/', (req, res) => {
-  helpers.getReactions(10155842254339728)
-    .then(reactions => {
-      res.send({
-        'result': 'SUCCESS',
-        'data': reactions
+  // todo: get active events, for each get reactions, drop into queue
+  let itemID = helpers.getItemIdFromURL('https://www.facebook.com/4ffrf/videos/10155842254339728/')
+  new Queue({'event_id': 1}).save()
+    .then(model => {
+      helpers.getReactions(itemID)
+      .then(reactions => {
+        let aggregate = helpers.aggregateReactions(reactions)
+        model.save({data: JSON.stringify(aggregate)}, {patch: true})
+        res.send({
+          'result': 'SUCCESS',
+          'data': aggregate
+        })
       })
-    })
-    .catch(error => {
-      console.log('wtf, mate?')
-      res.send({
-        'result': 'FAILURE',
-        'error': error
+      .catch(error => {
+        console.log('wtf, mate?')
+        res.send({
+          'result': 'FAILURE',
+          'error': error
+        })
       })
     })
 })
