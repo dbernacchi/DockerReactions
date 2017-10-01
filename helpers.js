fabda7df0d8887cf2d5163072e559f551e033ca9
Timothy Nott <timothy@ffrf.org>
2017-09-30 23:25:21 -0500

    created function to get reactions recursively

--- helpers.js
+++ helpers.js
@@ -1,7 +1,34 @@
+const axios = require('axios')
+const accessToken = '350787432040190|2tcZ-CcIcjvAjRn34FrmCgE1Yuw'
 helpers = {
-    fixAccessTokenInURL:function(url) {
-        return url.replace(/\u00257C/,"|")
-    }
+  test: function() {
+    return 'go!'
+  },
+  fixAccessTokenInURL:function(url) {
+    return url.replace(/\u00257C/,"|")
+  },
+  getReactions:function(itemID) {
+    console.log('starting getReactions')
+    let reactions = []
+    let starterUrl = 'https://graph.facebook.com/v2.10/' + itemID + '/reactions?access_token=' + accessToken + '&limit=10'
+    const getReactionsPage = url => axios.get(url)
+      .then(response => {
+        console.log('successful request', response)
+        reactions = reactions.concat(response.data.data)
+        if(response.data['paging']['next']) {
+          return getReactionsPage(response.data.paging.next)
+        } else {
+          return reactions
+        }
+      })
+      .catch(error => {
+        console.log('unsuccessful request', error)
+        reject(error)
+      })
+    console.log('starting recursion')
+
+    return getReactionsPage(starterUrl)
+  }
 }
-  
+
 module.exports = helpers
