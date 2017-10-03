5b21ccda968191ecc220711f2b30bc05c6106f8d
Timothy Nott <timothy@ffrf.org>
2017-10-02 22:00:51 -0500

    added UI and API for event log data

--- routes/events.js
+++ routes/events.js
@@ -2,8 +2,21 @@ const express = require('express')
 const router = express.Router()
 
 const Event = require('../models/event')
+const Log = require('../models/log')
 
 const helpers = require('../helpers')
+
+router.get('/:id/:startTime/:endTime', (req, res) => {
+  new Log({event_id: req.params.id})
+    .where('ts', '>=', req.params.startTime)
+    .where('ts', '<=', req.params.endTime)
+    .fetchAll()
+    .then(logs => {
+      res.send({
+        logs: logs.models
+      })
+    })
+})
 
 router.get('/', (req, res) => {
   helpers.getLiveEvents()
--- src/components/Events.vue
+++ src/components/Events.vue
@@ -4,9 +4,20 @@
       <h1>{{ event.name }}</h1>
       <form>
         <fieldset class="uk-fieldset">
+          <div class="uk-margin">
+            <input id="startTime" class="uk-input" :class="{'uk-form-danger': startTime.length == 0}" type="text" placeholder="Start Date/Time" v-model="startTime">
+          </div>
+          <div class="uk-margin">
+            <input id="endTime" class="uk-input" :class="{'uk-form-danger': endTime.length == 0}" type="text" placeholder="End Date/Time" v-model="endTime">
+          </div>
           <button :disabled="!validUpdateForm" :class="{ 'uk-button-primary': validUpdateForm, 'uk-button-disabled': !validUpdateForm }" class="uk-button uk-button-primary" v-on:click="update()" v-if="!isLoading">Update info</button>
         </fieldset>
       </form>
+      <ul class="uk-list">
+        <li v-for="(reaction, index) in reactions" :key="index">
+          {{ index }}: {{ reaction }}
+        </li>
+      </ul>
     </div>
     <div id="loading" v-if="isLoading" class="uk-position-cover uk-overlay-primary">
       <div class="uk-position-center">
@@ -26,10 +37,23 @@ export default {
   data () {
     return {
       isLoading: true,
+      endTime: '',
+      startTime: '',
       urlBase: 'http://localhost:7000/',
       logs: [],
       event: {
         name: ''
+      },
+      reactions: {
+        'NONE': 0,
+        'LIKE': 0,
+        'LOVE': 0,
+        'WOW': 0,
+        'HAHA': 0,
+        'SAD': 0,
+        'ANGRY': 0,
+        'THANKFUL': 0,
+        'PRIDE': 0
       }
     }
   },
@@ -39,7 +63,29 @@ export default {
   },
   computed: {
     validUpdateForm () {
-      return true
+      if(this.endTime.length > 0 && this.startTime.length > 0) {
+        return true
+      }
+      return false
+    }
+  },
+  methods: {
+    update () {
+      self = this
+      self.isLoading = true
+      axios.get(self.urlBase + 'events/' + self.$route.params.id + '/' + self.startTime + '/' + self.endTime)
+        .then( response => {
+          let maxIndex = response.data.logs.length - 1
+          for(let reaction in self.reactions) {
+            console.log(reaction, response.data.logs[maxIndex][reaction])
+            self.reactions[reaction] = response.data.logs[maxIndex][reaction] - response.data.logs[0][reaction]
+          }
+          self.isLoading = false
+        })
+        .catch( error => {
+          console.log(error)
+          self.isLoading = false
+        })
     }
   }
 }
