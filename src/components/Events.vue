e4225c0b82f13007f9588b03d7a4596d4dd8a249
Timothy Nott <timothy@ffrf.org>
2017-10-02 21:02:57 -0500

    added event route

---
+++ src/components/Events.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="event">
+    <div>
+      <h1>{{ event.name }}</h1>
+      <form>
+        <fieldset class="uk-fieldset">
+          <button :disabled="!validUpdateForm" :class="{ 'uk-button-primary': validUpdateForm, 'uk-button-disabled': !validUpdateForm }" class="uk-button uk-button-primary" v-on:click="update()" v-if="!isLoading">Update info</button>
+        </fieldset>
+      </form>
+    </div>
+    <div id="loading" v-if="isLoading" class="uk-position-cover uk-overlay-primary">
+      <div class="uk-position-center">
+        loading <span uk-icon="icon: clock; ratio: 1.5"></span>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+import UIKit from 'uikit'
+
+export default {
+  name: 'events',
+  data () {
+    return {
+      isLoading: true,
+      urlBase: 'http://localhost:7000/',
+      logs: [],
+      event: {
+        name: ''
+      }
+    }
+  },
+  mounted () {
+    this.event.name = this.$route.params.name
+    this.isLoading = false
+  },
+  computed: {
+    validUpdateForm () {
+      return true
+    }
+  }
+}
+</script>
+
+<style>
+  #loading {
+    font-size: 25px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/components/Home'
+import Events from '@/components/Events'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
   routes: [
-      {
-      path: '/',
-      name: 'Home',
-      component: Home
-      }
+    {
+    path: '/',
+    name: 'Home',
+    component: Home
+    },
+    {
+    path: '/events/:id/:name',
+    name: 'Events',
+    component: Events
+    }
   ],
-  linkActiveClass: 'uk-active'
+linkActiveClass: 'uk-active'
 })
